f9eb46c4389f5a39a23a358291dd250ef9325c13
Kayleigh Bellis <bellis.kayleigh@gmail.com>
2018-07-18 17:31:29 +0100

    Adding intial files and renaming test folder to have it without the s

---
+++ test/test_etl_system.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestEtlSystem(unittest.TestCase):
+    """
+    This class holds all the tests for each method within the elt_system class
+    """
+
+    def test_read_file(self):
+        """
+
+        :return:
+        """
---
+++ yieldify-exercise/etl_system.py
@@ -0,0 +1,16 @@
+import pandas
+
+
+class etl_metrics:
+    """This class Runs a ETL service, which takes in the data and produces top 5 metrics """
+
+    def __init__(self):
+        """
+        """
+
+
+    def read_file(self):
+        """
+
+        :return: Pandas dataframe
+        """
