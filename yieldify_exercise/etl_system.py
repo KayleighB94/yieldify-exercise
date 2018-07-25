9035add82e51ddf98960d5fb9d235473e9af3a19
kayleigh94 <kayleigh.bellis@hotmail.co.uk>
2018-07-25 02:21:37 +0100

    Adding doc strings and adding to the methods

--- yieldify_exercise/etl_system.py
+++ yieldify_exercise/etl_system.py
@@ -1,11 +1,15 @@
+import sys
+import getopt
 import pandas
 import httpagentparser
 from geolite2 import geolite2
+
 class ETL_Metrics:
     """This class Runs a ETL service, which takes in the data and produces top 5 metrics """
 
     def __init__(self):
         """
+        This method initializes three of the objects used throughout the class.
         """
         self.path = []
         self.top_5 = None
@@ -17,16 +21,22 @@ class ETL_Metrics:
         The method creates a list of strings, which will be used for columns names:
             ["date", "time", "user_id", "url", "IP", "user_agent_string"]
 
-        This is then used along with the path
+        This is then used along with the path to read in a gzip file to a pandas table.
         """
         columns = ["date", "time", "user_id", "url", "IP", "user_agent_string"]
-        self.df = pandas.read_table(self.path, compression= "gzip",names=columns)
+        self.df = pandas.read_table(self.path, compression="gzip", names=columns)
 
 
     def setup_data(self):
         """
+        This method uses the classes pandas dataframe and saves back to it.
 
-        :return:
+        It first initializes the connection with geolite2, then for each IP in the data, it finds out the country and
+        city of that IP and stores this as a new column onto our pandas dataframe. After it has does this we close the
+        connection to geolite2.
+
+        After that will call httpagentparser which converts our user_agent_string into its browser family and it's OS,
+        this is done for each row and is saved as new columns onto our pandas dataframe.
         """
         reader = geolite2.reader()
         self.df["country"] = self.df.apply(lambda k:reader.get(k["IP"])['country']['names']['en'], axis=1)
@@ -37,35 +47,83 @@ class ETL_Metrics:
 
 
 
-    def compute_top_5(self, groupByCols):
-        """"
+    def compute_top(self, groupByCols, unique=None):
         """
-        top_5 = self.df.groupby(groupByCols).size().reset_index(name='count')
-        return top_5
+        This method first checks whether we are counting the unique rows of a column or counting all the rows,
+        this is done through the unique parameter. If it hasn't been passed in then it defaults to None and the
+        dataframe is then grouped by the groupByCols parameter and the rows are counted.
+
+        If it has been passed then the dataframe is then grouped by the groupByCols parameter and the number of unique
+        values in the unique parameter column is counted.
+
+        The dataframe is then returned.
+
+        :param groupByCols: List[Strings] - List of the column names you want to group by
+        :param unique: String - Name of column to count unique values of, defaults to None if you want to count all the
+                                rows
+        :return: Pandas DataFrame
+        """
+        if unique is None:
+            grouped_df = self.df.groupby(groupByCols).size().reset_index(name='count')
+        else:
+            grouped_df = self.df.groupby(groupByCols)[unique].nunique()
+        return grouped_df
 
 
-    def run(self):
+    def main(self):
         """
+        This method is the main one which first gets the arguments passed in through the command line. Then it calls
+        the other methods in order and prints out the statistics of the top 5 of the following:
+        Countries per event (row)
+        Cities per event (row)
+        Browser family based on unique users
+        OS based on unique users
 
-        :return:
+
         """
+        ## Creating a help string, if the user inputs the wrong parameters or no parameters
+        help_str = "etl_system.py --h <help> --path <path_to_input_data>"
+        try:
+            opts, args =  getopt.getopt(sys.argv[1:], "hpath")
+        except getopt.GetoptError as err:
+            print(err)
+            print(help_str)
+            sys.exit(2)
+
+        for opt, arg in opts:
+            if opt in ("-h", "--help"):
+                print(help_str)
+                sys.exit()
+            elif opt in ("-p", "--path"):
+                self.path = arg
+            else:
+                print("Unhandled Options")
+                print(help_str)
+
+        # Calling two of the method that read in the data and convert the columns from ip and user string to
+        # countries, cities, browsers and os'
         self.read_file()
         self.setup_data()
 
         ## Top 5 per event
-        top_5_countries = self.compute_top_5(["country"])
-        top_5_cities = self.compute_top_5(["city"])
+        top_5_countries = self.compute_top(["country"])
+        top_5_cities = self.compute_top(["city"])
 
         ## Top 5 per unquie user
-        top_5_browser_per = self.compute_top_5(["browser"])
-        top_5_os_per = self.compute_top_5(["os"])
+        top_5_browser_per = self.compute_top(["browser"], "user")
+        top_5_os_per = self.compute_top(["os"], "user")
 
-        print("Top 5 Countries per event")
-        print(top_5_countries)
-        return top_5_countries
+        print("Top 5 Countries per event:")
+        print(top_5_countries.sort_values("count", ascending=False).head())
+        print("Top 5 Countries per event:")
+        print(top_5_cities.sort_values("count", ascending=False).head())
+        print("Top 5 Countries per event:")
+        print(top_5_browser_per.sort_values("count", ascending=False).head())
+        print("Top 5 Countries per event:")
+        print(top_5_os_per.sort_values("count", ascending=False).head())
 
 
 if __name__ == "__main__":
     print("ETL pipeline for Top 5 metrics")
     print("These are the following parameters used: ")
-    ETL_Metrics().run()
+    ETL_Metrics().main()
